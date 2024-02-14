53b015c9728ff6d9d6da4a8b1fa0b969f0d11289
kamal joshi <kamaljoshi673243@gmail.com>
2024-02-14 21:57:23 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,45 +1,60 @@
 import "./App.css";
 import NavBar from "./Components/NavBar.js";
 import ProductList from "./Components/ProductList.js";
+import React, { useState } from "react";
 
 function App() {
-  const productList = [
+  const products = [
     {
       name: "Vivo",
       price: 2200,
-      quantity: 2
+      quantity: 0
     },
     {
       name: "iPhone",
       price: 2000,
-      quantity: 2
+      quantity: 0
     },
     {
       name: "Realme",
       price: 2300,
-      quantity: 2
+      quantity: 0
     },
     {
       name: "Lava",
       price: 1400,
-      quantity: 2
+      quantity: 0
     },
     {
       name: "Oppo",
       price: 1800,
-      quantity: 2
+      quantity: 0
     },
     {
       name: "Redme",
       price: 2000,
-      quantity: 2
+      quantity: 0
     },
   ]
+
+  let [productList, setProductList] = useState(products)
+
+  const incrementQuantity = (index) => {
+    let newProductList = [...productList]
+    newProductList[index].quantity++
+    setProductList(newProductList)
+  }
+
+  const decrementQuantity = (index) => {
+    let newProductList = [...productList]
+    newProductList[index].quantity > 0 ? newProductList[index].quantity-- : newProductList[index].quantity = 0
+    setProductList(newProductList)
+  }
   return (
     <>
       <NavBar />
       <main class="container mt-5">
-        <ProductList productList={productList} />
+        <ProductList productList={productList} incrementQuantity={incrementQuantity} decrementQuantity={decrementQuantity} />
       </main>
     </>
   );
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,16 +1,15 @@
 import React from 'react'
 
 export default function Product(props) {
-  console.log(props)
   return (
     <div className='row'>
       <div className='col-3'>
         <h2>{props.product.name}  <span class="badge bg-secondary">₹{props.product.price}</span></h2>
       </div>
       <div className="col-3">
-        <button type="button" class="btn btn-outline-info w-25"><b>+</b></button>
+        <button type="button" class="btn btn-outline-info w-25" onClick={() => { props.decrementQuantity(props.index) }}><b>-</b></button>
         <button type="button" class="btn btn-outline-primary col-2">{props.product.quantity}</button>
-        <button type="button" class="btn btn-outline-info w-25"><b>-</b></button>
+        <button type="button" class="btn btn-outline-info w-25" onClick={() => { props.incrementQuantity(props.index) }}><b>+</b></button>
       </div>
       <div className="col-2">
         <h3>{props.product.price * props.product.quantity}</h3>
